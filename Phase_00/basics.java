4f86963756d4643c1fa2ff59b1abe0def5f52e60
AbhishekYadav-09 <abhishekyadavcode@gmail.com>
2026-02-20 04:13:10 +0530

    Start from basics

---
+++ Phase_00/basics.java
@@ -0,0 +1,16 @@
+package Phase_00;
+
+public class basics {
+    public static void main(String[] args) {
+        System.out.println("Hello PW");
+        System.out.println("UPSC Wallah");
+        System.out.println("     SSC Wallah    ");
+
+        System.out.println("4");
+        System.out.println(4);
+        System.out.println(4 + 3);
+        System.out.println("4+3");
+
+    }
+}
+
