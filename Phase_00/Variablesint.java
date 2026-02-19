dec3e2d71a5f3a26bb53e808c37192ffac001c4a
AbhishekYadav-09 <abhishekyadavcode@gmail.com>
2026-02-20 04:24:03 +0530

    Printing Variables in Java & Upsation of Variables

--- Phase_00/Variablesint.java
+++ Phase_00/Variablesint.java
@@ -1,1 +1,26 @@
+package Phase_00;
+
+// Variables and theair Declaration
+ 
+// => let os focus on int data type as of now
+
+//  1) Variables
+public class Variablesint {
+    public static void main(String[] args){
+        int x;
+        x = 500;
+        // System.out.println(x);
+        // x = 400;
+        // System.out.println(x);
+        // System.out.println(x+1);
+
+        System.out.println(x);
+        x = x + 5;
+        System.out.println(x);
+        x = x + 10;
+        System.out.println(x);
+        x = x - 10;
+        System.out.println(x);
+    }
+}
 
