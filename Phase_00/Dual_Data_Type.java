1a9b55017be46dcaea5c69cb02d6d087b6c76649
AbhishekYadav-09 <abhishekyadavcode@gmail.com>
2026-02-23 16:46:36 +0530

    DualDataType

---
+++ Phase_00/Dual_Data_Type.java
@@ -0,0 +1,13 @@
+package Phase_00;
+
+public class Dual_Data_Type {
+    public static void main(String[] args){
+        int x =5;
+        int y = 2;
+        System.out.println(x/y);
+
+        double a = 5;
+        double b = 2;
+        System.out.println(a/b);
+    }
+}
